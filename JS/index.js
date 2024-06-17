703ecf6f1ca609a3ae08ec17651f2eff597d17e8
Timmy <hatem.bassah@gmail.com>
2024-06-17 05:10:16 +0300

    First Draft of the MetaBlog Application

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const wrapper = document.querySelector('.wrapper');
+    const registerLink = document.querySelector('.register-link');
+    const loginLink = document.querySelector('.login-link');
+
+    registerLink.addEventListener('click', () => {
+        wrapper.classList.add('active');
+    });
+
+    loginLink.addEventListener('click', () => {
+        wrapper.classList.remove('active');
+    });
+});
+
+function navigateToHome() {
+    window.location.href = 'index.html';
+}
